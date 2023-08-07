5d39f98c54e2b62e71762b8fd5765d4d84c38865
Aluno <aluno@etec.sp.gov.br>
2023-08-07 11:44:40 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Text, TextInput, Button, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () =>{
+  const [inputValues, setInputValues] = useState({
+    input: '',
+    
+  });
+  const [sum, setSum] = useState(0);
+  const handleInputChange = (inputName) => {
+    setInputValues({...inputValues, [inputName]: value});
 
-const styles = StyleSheet.create({
-  container: {
+  };
+  const handlesSum = () =>{
+    const {imput} = inputValues;
+    const result = parseInt(input1);
+    setSum(result);
+  };
+
+  return(
+    <View style={Styles.container}>
+    <Text style={styles.label}>Input: </Text>
+    <TextInput
+    style={styles.input}
+    onChangeText={(value) => handleInputChange('input1', value)}
+    value={inputValues.input1}
+    keyboardType="numeric"
+  />
+
+<Button title="Somar" onPress={handlesSum}/>
+<Text style={styles.result}>Resultado: {sum}</Text>
+
+
+
+  </View>
+  )
+
+};
+
+const styles=StyleSheet.create = ({
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundcolor: '#2CDA9D',
+    padding: 20,
   },
+
 });
+
+export default App;
+
+
