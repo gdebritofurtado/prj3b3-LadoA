c990de3095c6ce69c955af2472d4a69e762a8d62
Geovana de Brito Furtado <125288359+gdebritofurtado@users.noreply.github.com>
2023-08-14 12:13:15 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,48 +1,70 @@
-import { View, Text, Text, TextInput, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
-const App = () =>{
+const App = () => {
   const [inputValues, setInputValues] = useState({
-    input: '',
-    
+    input1: '', // Corrigido para "input1"45
+    input2:'', 
   });
   const [sum, setSum] = useState(0);
-  const handleInputChange = (inputName) => {
-    setInputValues({...inputValues, [inputName]: value});
 
+  const handleInputChange = (inputName, value) => { // Corrigido para passar o valor
+    setInputValues({ ...inputValues, [inputName]: value });
   };
-  const handlesSum = () =>{
-    const {imput} = inputValues;
-    const result = parseInt(input1);
+
+  const handleSum = () => {
+    const { input1,input2 } = inputValues;
+    const result = parseInt(input1) + parseInt(input2); // Lidando com entrada vazia
     setSum(result);
   };
 
-  return(
-    <View style={Styles.container}>
-    <Text style={styles.label}>Input: </Text>
-    <TextInput
-    style={styles.input}
-    onChangeText={(value) => handleInputChange('input1', value)}
-    value={inputValues.input1}
-    keyboardType="numeric"
-  />
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Input: </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(value) => handleInputChange('input1', value)}
+        value={inputValues.input1}
+        keyboardType="numeric"
+      />
+      
+      <Text style={styles.label}>Input2: </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(value) => handleInputChange('input2', value)}
+        value={inputValues.input2}
+        keyboardType="numeric"
+      />
 
-<Button title="Somar" onPress={handlesSum}/>
-<Text style={styles.result}>Resultado: {sum}</Text>
-
-
-
-  </View>
-  )
-
+      <Button title="Somar" onPress={handleSum} />
+      <Text style={styles.result}>Resultado: {sum}</Text>
+    </View>
+  );
 };
 
-const styles=StyleSheet.create = ({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    backgroundcolor: '#2CDA9D',
+    backgroundColor: '#2CDA9D', // Corrigido para "backgroundColor"
     padding: 20,
   },
-
+  label: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 4,
+    padding: 8,
+    marginBottom: 10,
+  },
+  result: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 20,
+  },
 });
 
 export default App;
